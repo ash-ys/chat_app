bf3fe6f296353a160bdc3b923e83e51ad288c627
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-09-03 22:18:30 +0545

    error

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
         <ChatEngine
         height = "100vh"
         projectID="6851fa0e-879a-4849-a754-d7bdb8a130a7"
-        userName="Ash"
+        userName="Ashish"
         userSecret="123123"
         renderChatFeed = {(chatAppProps)=> <ChatFeed{...chatAppProps}/> }
         />
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 const LoginForm = () => {
     const [username, setUsername]= useState('');
     const [password, setPassword] = useState('');
+    const [error, setErrot] = useState('');
 
     const handleSubmit = async (e) =>{
         e.preventDefault();
@@ -18,11 +19,8 @@ const LoginForm = () => {
             window.location.reload();
         }catch(error){
             //error-> try again
+            setErrot('Opps, Wrong Information!')
         }
-        
-        
-        
-        
     }
 
     return (
@@ -37,6 +35,7 @@ const LoginForm = () => {
                             <span>Start Chatting</span>
                         </button>
                     </div>
+                    <h2 className="error">{error}</h2>
                 </form>
             </div>
             </div>
