0680fac572bbbda92ca53e311c5ebf106eb3f408
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-09-03 16:24:23 +0545

    chat

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,11 +1,48 @@
-
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
 const ChatFeed =(props )=>{
-console.log(props);
-return(
-    <div>
-        ChatFeed
+
+    const {chats, activeChat, userName, messages} = props;
+
+    const chat = chats && chats[activeChat];
+
+    const renderMessages = ()=>{
+        const keys = Object.keys(messages);
+        console.log(keys);
+        return keys.map(key,index) => {
+            const message = messages[key];
+            const LastMessageKey = index===0 ? null: keys[index -1];
+            const isMyMessage = userName === message.sender.username;
+            
+            return(
+                <div key = {`msg_${index}`} style = {{width: '100%'}}>
+                    <div className = "message-block">
+                        {
+                            isMyMessage
+                            ? <MyMessage />
+                            : <TheirMessage />
+                        }
+                    </div>
+                    <div className="read-receipts" style={{marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMessage ? '0px' : '68px' }}>
+                    read-receipts
+
+                    </div>
+
+                </div>
+            );
+        
+        })
+
+    }
+    renderMessages()
+
+
+    return(
+        <div>
+            ChatFeed
         </div>
-);
+    );
 }
 
 export default ChatFeed;
