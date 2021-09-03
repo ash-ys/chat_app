ad3a17760dc32ffd098ae802a838ba4619a90c19
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-09-03 13:36:47 +0545

    user added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ const App = () => {
     return(
         <ChatEngine
         height = "100vh"
-        projectID=""
-        userName=""
-        userSecret=""
+        projectID="6851fa0e-879a-4849-a754-d7bdb8a130a7"
+        userName="Ash"
+        userSecret="123123"
         />
-    )
+    );
 }
 
 export default App;
