8c19654b62ac682655ca4d30c3d8a73b894260af
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-09-03 12:41:57 +0545

    app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,13 @@
 import {ChatEngine} from 'react-chat-engine';
 import './App.scss';
+
+const App = () => {
+    return(
+        <ChatEngine
+        height = "100vh"
+        projectID=""
+        userName=""
+        userSecret=""
+        />
+    )
+}
